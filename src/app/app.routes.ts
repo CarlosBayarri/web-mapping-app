c08fc379ad65ebfce31c2ff5fad68d55bad11fe6
Carlos Bayarri Cebrecos <cbayarri@disid.com>
2024-01-29 16:38:54 +0100

    feat: add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./nx-welcome.component').then((m) => m.NxWelcomeComponent),
+  },
+  {
+    path: 'openlayers',
+    loadComponent: () => import('./openlayers/openlayers.component').then((m) => m.OpenlayersComponent),
+  },
+  {
+    path: 'leaflet',
+    loadComponent: () => import('./leaflet/leaflet.component').then((m) => m.LeafletComponent),
+  },
+  {
+    path: 'mapbox',
+    loadComponent: () => import('./mapbox/mapbox.component').then((m) => m.MapboxComponent),
+  },
+  {
+    path: 'google',
+    loadComponent: () => import('./google/google.component').then((m) => m.GoogleComponent),
+  }
+];
